b8ad7ff04b5e13e32c0a53a9e7ca0cd3eb428d75
Shubham <shubham@logisticinfotech.co.in>
2020-07-25 10:57:15 +0530

    -- Design Changes

--- src/Alert/AlertStyle.js
+++ src/Alert/AlertStyle.js
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
 import { Responsive, Colors } from "../Theme";
 
+// Lib
+import { getStatusBarHeight } from "react-native-status-bar-height";
+
 export const styles = StyleSheet.create({
   container: {
     position: "absolute",
@@ -8,39 +11,40 @@ export const styles = StyleSheet.create({
     left: 0,
     right: 0,
     zIndex: 10000,
-    elevation: 10000
+    elevation: 10000,
   },
   alertBGContainer: {
     paddingHorizontal: Responsive.widthPercentageToDP("8%"),
-    paddingTop: Responsive.heightPercentageToDP("7%"),
-    paddingBottom: Responsive.heightPercentageToDP("3%")
+    paddingTop:
+      Responsive.heightPercentageToDP("3%") + getStatusBarHeight(true),
+    paddingBottom: Responsive.heightPercentageToDP("3%"),
   },
   alertMainContainer: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
   },
   imageStyle: {
     marginRight: Responsive.widthPercentageToDP("5%"),
-    aspectRatio: 1
+    aspectRatio: 1,
   },
   imageMainStyle: {
     width: "100%",
-    height: "100%"
+    height: "100%",
   },
   alertTextContainer: {
-    flex: 1
+    flex: 1,
   },
   alertTitleStyle: {
     fontSize: Responsive.convertFontScale("18"),
     fontWeight: "900",
     color: Colors.white,
-    marginVertical: Responsive.heightPercentageToDP("0.5%")
+    marginVertical: Responsive.heightPercentageToDP("0.5%"),
   },
   alertMessageStyle: {
     fontSize: Responsive.convertFontScale("15"),
     fontWeight: "500",
     color: Colors.white,
-    marginVertical: Responsive.heightPercentageToDP("0.5%")
+    marginVertical: Responsive.heightPercentageToDP("0.5%"),
   },
   alertButtonStyle: {
     width: Responsive.widthPercentageToDP("18%"),
@@ -49,18 +53,18 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: Colors.white,
     borderRadius: Responsive.heightPercentageToDP("2%"),
-    marginHorizontal: Responsive.widthPercentageToDP("2%")
+    marginHorizontal: Responsive.widthPercentageToDP("2%"),
   },
   alertButtonContainer: {
     alignItems: "center",
     flexDirection: "row",
-    marginTop: Responsive.heightPercentageToDP("1%")
+    marginTop: Responsive.heightPercentageToDP("1%"),
   },
   alertButtonTextStyle: {
     fontSize: Responsive.convertFontScale(12),
     fontWeight: "500",
-    color: Colors.black
-  }
+    color: Colors.black,
+  },
 });
 
 export default { styles };
